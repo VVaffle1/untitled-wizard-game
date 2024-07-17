8b7a3710f3b1f0afd615374510bc397a7f13a1ec
dGWingnut <vaughninman@gmail.com>
2024-07-17 15:56:03 -0700

    added wave functionalty
    make sure to add an empty object (with the script attached to it) with invisible boxes inside to give them points to spawn at

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,35 +6,38 @@ public class EnemySpawn : MonoBehaviour
 {
     public GameObject Enemy;
     public float timeToSpawn, spawnCountdown;
+    public int SpawnPerWave = 1;
 
-    // Start is called before the first frame update
     void Start()
     {
-        spawnCountdown = timeToSpawn;
+        spawnCountdown = timeToSpawn / 2;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //StartCoroutine(SpawnEnemy());
         spawnCountdown -= Time.deltaTime;
 
         if(spawnCountdown <= 0)
         {
-            Instantiate(Enemy, transform.position, Quaternion.identity);
+            for(int i = 0; i < SpawnPerWave; i++)
+            {
+                int randomChild = Random.Range(0, transform.childCount);
+                Transform FoundChild = transform.GetChild(randomChild);
+
+                Instantiate(Enemy, GetRandomPoint(FoundChild), Quaternion.identity);
+            }
             spawnCountdown = timeToSpawn;
+
+            SpawnPerWave += Random.Range(1, 2);
         }
     }
 
-    //IEnumerator SpawnEnemy()
-    //{
-    //    while (canSpawn == true) 
-    //    {
-            
-    //        canSpawn = false;
-    //        yield return new WaitForSeconds(3);
-    //        canSpawn = true;
-    //    }
-        
-    //}
+    Vector3 GetRandomPoint(Transform FromTransform)
+    {
+        float Xp = FromTransform.localScale.x;
+        float Yp = FromTransform.localScale.y;
+        Xp = Random.Range(-Xp / 2, Xp / 2);
+        Yp = Random.Range(-Yp / 2, Yp / 2);
+        return new Vector3(FromTransform.position.x + Xp, FromTransform.position.y + Yp, 0);
+    }
 }
