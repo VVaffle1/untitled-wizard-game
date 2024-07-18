eb5793e930b997675ca6afc61352c3995bb405e8
Davy Forrester <58455330+RoyalDucky@users.noreply.github.com>
2024-07-17 21:43:09 -0700

    Implemented basic wave timer

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] float timerCountdown = 60;
+    public WaveManagerScript waveDisplay;
+    [SerializeField] private bool canCount = false;
+    [SerializeField] GameObject waveTextBox; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = timerCountdown.ToString("F0");
+
+        if(timerCountdown <= 0)
+        {
+            waveDisplay.waveNumber++;
+            timerCountdown = 60;
+            canCount = false;
+            waveTextBox.SetActive(true);
+            waveDisplay.Awake();
+        }
+
+        if(canCount == true && timerCountdown > 0)
+        {
+            timerCountdown -= Time.deltaTime;
+        }
+    }
+
+    public void StartTimer()
+    {
+            waveTextBox.SetActive(false);
+            canCount= true;
+       
+    }
+}
---
+++ Assets/Scripts/WaveManagerScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Globalization;
+using System.Threading;
+
+public class WaveManagerScript : MonoBehaviour
+{
+    public float waveNumber = 1;
+    public TimerScript timerText;
+
+    // Start is called before the first frame update
+    public void Awake() 
+    {
+        StartCoroutine(DisplayWave());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = "Wave: " + waveNumber.ToString();
+    }
+
+    public IEnumerator DisplayWave()
+    {
+        yield return new WaitForSeconds(2f);
+        timerText.StartTimer();
+    }
+}
