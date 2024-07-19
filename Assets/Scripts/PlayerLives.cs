b5cc1c75b93be17ecaf40141d77ee6169492fdf4
VVaffle1 <wolfewsmith@gmail.com>
2024-07-19 15:45:57 -0700

    Knockback 2

--- Assets/Scripts/PlayerLives.cs
+++ Assets/Scripts/PlayerLives.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,29 +7,31 @@ using UnityEngine.SceneManagement;
 public class PlayerLives : MonoBehaviour
 {
     public int playerHealth = 3;
-    public SceneLoader sceneControl; 
+    public SceneLoader sceneControl;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float knockbackDuration = 0.2f;
+    public float knockbackSpeed = 10f;
 
-    // Update is called once per frame
+
     void Update()
     {
-        if(playerHealth <= 0)
+        if (playerHealth <= 0)
         {
-           StartCoroutine(GameOverTime());
-            
+            StartCoroutine(GameOverTime());
+
         }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "Enemy")
+        if (other.tag == "Enemy")
         {
+
             playerHealth--;
+            if (playerHealth <= 0) { return; }
+            Vector3 difference = (transform.position - other.transform.position).normalized;
+
+            StartCoroutine(Knockback(difference));
         }
     }
 
@@ -36,5 +39,18 @@ public class PlayerLives : MonoBehaviour
     {
         yield return new WaitForSeconds(0.1f);
         FindObjectOfType<SceneLoader>().GameOver();
+    }
+
+    IEnumerator Knockback(Vector3 direction)
+    {
+        float timer = 0f;
+
+        while (timer < knockbackDuration)
+        {
+            Debug.Log("worked");
+            transform.position += direction * knockbackSpeed * Time.deltaTime;
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
