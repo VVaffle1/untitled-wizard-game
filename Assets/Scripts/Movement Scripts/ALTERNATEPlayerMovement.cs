01fcf2ee03fe8b2cbfc816d9d4d27699b0565596
Davy Forrester <58455330+RoyalDucky@users.noreply.github.com>
2024-07-19 00:28:21 -0700

    Fully implemented game over screen

--- Assets/Scripts/Movement Scripts/ALTERNATEPlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/ALTERNATEPlayerMovement.cs
@@ -12,6 +12,8 @@ public class ALTERNATEPlayerMovement : MonoBehaviour
 
     public Animator anim;
 
+    private bool touchesGrass;
+
     // Update is called once per frame
     void Update()
     {
@@ -21,10 +23,35 @@ public class ALTERNATEPlayerMovement : MonoBehaviour
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (touchesGrass == true)
+        {
+            moveSpeed = 2f;
+        }
+        else
+        {
+            moveSpeed = 4f;
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Grass")
+        {
+            touchesGrass = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Grass")
+        {
+            touchesGrass = false;
+        }
     }
 }
